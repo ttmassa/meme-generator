903477ad34c570e0978841c65728735cf5fd7aad
Massa Timothée <timothee.massa.perso@gmail.com>
2023-11-02 00:28:41 +0100

    Header styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import React from 'react'
+import trollFace from '../images/troll-face.png'
 
 export default function Header() {
     return (
         <header className='header--container'>
-            <div className='header--title'>
-                <img 
-                    src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
-                    alt="Problem?"
-                    className='header--troll-face'/>
-                <h1 className='header--meme-generator'>Meme Generator</h1>
-            </div>
+        <img 
+            src={trollFace}
+            alt="Problem?"
+            className='header--troll-face'/>
+        <h1 className='header--title'>Meme Generator</h1>
             <h3 className='header--description'>React Course - Project 3</h3>
         </header>
     )
