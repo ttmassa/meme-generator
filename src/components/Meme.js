08a0991e865486260dc93d27c60030a842d9b6d6
Massa Timothée <timothee.massa.perso@gmail.com>
2023-11-07 19:53:50 +0100

    Meme input texts added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,9 +3,22 @@ import React from 'react'
 export default function Meme() {
     return (
         <form className='meme--container'>
-            <input />
-            <input />
-            <button/>
+            <input 
+            className='meme--input'
+            type='text'
+            autoFocus
+            placeholder='Top Text'
+            />
+            <input 
+            className='meme--input'
+            type='text'
+            placeholder='Bottom Text'
+            />
+            <button 
+            className='meme--button'
+            type='submit'
+            >Get a new meme image  🖼</button>
+            
         </form>
     )
 }
