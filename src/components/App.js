5259f056231d9ebcdb74c159016124ae57c0c334
Massa Timothée <timothee.massa.perso@gmail.com>
2023-11-07 19:47:35 +0100

    Meme component created and linked to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../style.css'
 import Header from './Header'
+import Meme from './Meme'
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Meme />
         </div>
     )
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Meme() {
+    return (
+        <form className='meme--container'>
+            <input />
+            <input />
+            <button/>
+        </form>
+    )
+}
