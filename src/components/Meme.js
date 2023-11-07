405711046c016ef28f374604d17343de2974ac11
Massa Timothée <timothee.massa.perso@gmail.com>
2023-11-07 20:36:25 +0100

    Meme component style finished

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,23 +2,25 @@ import React from 'react'
 
 export default function Meme() {
     return (
-        <form className='meme--container'>
-            <input 
-            className='meme--input'
-            type='text'
-            autoFocus
-            placeholder='Top Text'
-            />
-            <input 
-            className='meme--input'
-            type='text'
-            placeholder='Bottom Text'
-            />
-            <button 
-            className='meme--button'
-            type='submit'
-            >Get a new meme image  🖼</button>
-            
-        </form>
+        <main className='meme--container'>
+            <form className='meme--form'>
+                <input 
+                className='meme--input'
+                type='text'
+                autoFocus
+                placeholder='Top Text'
+                />
+                <input 
+                className='meme--input'
+                type='text'
+                placeholder='Bottom Text'
+                />
+                <button 
+                className='meme--button'
+                type='submit'
+                >Get a new meme image  🖼</button>
+            </form>
+        </main>
+
     )
 }
