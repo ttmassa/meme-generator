1717917532247f6936aa90e8abd508a3074f3c66
Massa Timothée <timothee.massa.perso@gmail.com>
2023-12-06 00:15:34 +0100

    handleClick returns a random meme url

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,24 +1,36 @@
 import React from 'react'
+import memesData from '../memesData.js'
 
 export default function Meme() {
+    function handleClick() {
+        const memesArray = memesData.data.memes;
+        const randomMeme = memesArray[Math.floor(Math.random() * memesArray.length)];
+        console.log(randomMeme.url);
+        
+    }
+
     return (
         <main className='meme--container'>
             <div className='meme--form'>
                 <input 
-                className='meme--input'
-                type='text'
-                autoFocus
-                placeholder='Top Text'
+                    className='meme--input'
+                    type='text'
+                    autoFocus
+                    placeholder='Top Text'
                 />
                 <input 
-                className='meme--input'
-                type='text'
-                placeholder='Bottom Text'
+                    className='meme--input'
+                    type='text'
+                    placeholder='Bottom Text'
                 />
                 <button 
-                className='meme--button'
-                type='submit'
-                >Get a new meme image  🖼</button>
+                    className='meme--button'
+                    type='submit'
+                    onClick={handleClick}
+
+                >
+                    Get a new meme image  🖼
+                </button>
             </div>
         </main>
 
